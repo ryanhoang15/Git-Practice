9993d2a9c650dd5587c729ccd09b19f5a7738118
Ryan Hoang <ryanhoang15@gmail.com>
2021-02-24 18:24:03 -0800

    Addind to Git-Practice Repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by ryan hoang on 8/17/20.
+//  Copyright © 2020 ryan hoang. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    return 0;
+}
